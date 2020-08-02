8729dbc7806f405080fc67dda3c17f5062a44a93
Dan <danilvoytvoich07@gmail.com>
2020-08-02 12:08:59 +0300

    update

--- database.py
+++ database.py
@@ -20,8 +20,8 @@ cursor.execute("""
 		level BIGINT DEFAULT 1 NOT NULL,
 		exp BIGINT DEFAULT 0 NOT NULL,
 		robux BIGINT DEFAULT 0 NOT NULL,
-		roblox_id BIGINT,
-		job_id SMALLINT,
+		roblox_id BIGINT DEFAULT NULL,
+		job_id SMALLINT DEFAULT NULL,
 		inventory TEXT DEFAULT '[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]' NOT NULL
 	);
 
@@ -38,11 +38,11 @@ cursor.execute("""
 """)
 
 def add_guild(guild_id):
-	cursor.execute("INSERT INTO guilds (guild_id) VALUES (%s)", (guild_id,))
+	cursor.execute("INSERT INTO guilds (guild_id) VALUES (%s);", (guild_id,))
 	db.commit()
 
 def get_guild(guild_id):
-	cursor.execute("SELECT * FROM guilds WHERE guild_id = %s", (guild_id,))
+	cursor.execute("SELECT * FROM guilds WHERE guild_id = %s;", (guild_id,))
 	result = cursor.fetchone()
 
 	if result == None:
@@ -52,11 +52,11 @@ def get_guild(guild_id):
 	return result
 
 def add_user(user_id):
-	cursor.execute("INSERT INTO users (user_id) VALUES (%s)", (user_id,))
+	cursor.execute("INSERT INTO users (user_id) VALUES (%s);", (user_id,))
 	db.commit()
 
 def get_user(user_id):
-	cursor.execute("SELECT * FROM users WHERE user_id = %s", (user_id,))
+	cursor.execute("SELECT * FROM users WHERE user_id = %s;", (user_id,))
 	result = cursor.fetchone()
 
 	if result == None:
@@ -66,27 +66,27 @@ def get_user(user_id):
 	return result
 
 def get_all_users():
-	cursor.execute("SELECT * FROM users")
+	cursor.execute("SELECT * FROM users;")
 	return cursor.fetchall()
 
 def get_top10_users(order_by, limit):
-	cursor.execute(f"SELECT * FROM users ORDER BY {order_by} DESC LIMIT 10")
+	cursor.execute(f"SELECT * FROM users ORDER BY {order_by} DESC LIMIT 10;")
 	return cursor.fetchall()
 
 def add_giveaway(guild_id, channel_id, message_id, reward, winners, requirements, ends_at):
-	cursor.execute("INSERT INTO giveaways (guild_id, channel_id, message_id, reward, winners, requirements, ends_at) VALUES (%s, %s, %s, %s, %s, %s, %s)", (guild_id, channel_id, message_id, reward, winners, requirements, ends_at,))
+	cursor.execute("INSERT INTO giveaways (guild_id, channel_id, message_id, reward, winners, requirements, ends_at) VALUES (%s, %s, %s, %s, %s, %s, %s);", (guild_id, channel_id, message_id, reward, winners, requirements, ends_at,))
 	db.commit()
 
 def get_giveaway(guild_id, channel_id, message_id):
-	cursor.execute("SELECT * FROM giveaways WHERE guild_id = %s AND channel_id = %s AND message_id = %s", (guild_id, channel_id, message_id,))
+	cursor.execute("SELECT * FROM giveaways WHERE guild_id = %s AND channel_id = %s AND message_id = %s;", (guild_id, channel_id, message_id,))
 	return cursor.fetchone()
 
 def get_all_giveaways():
-	cursor.execute("SELECT * FROM giveaways")
+	cursor.execute("SELECT * FROM giveaways;")
 	return cursor.fetchall()
 
 def get_guild_giveaways(guild_id):
-	cursor.execute("SELECT * FROM giveaways WHERE guild_id = %s", (guild_id,))
+	cursor.execute("SELECT * FROM giveaways WHERE guild_id = %s;", (guild_id,))
 	return cursor.fetchall()
 
 def delete_giveaway(guild_id, channel_id, message_id):
