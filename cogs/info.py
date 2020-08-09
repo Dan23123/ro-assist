014795e856a2397785476a37c90a10dc29b9269c
Dan <danilvoytvoich07@gmail.com>
2020-08-09 19:31:25 +0300

    updateg

--- cogs/info.py
+++ cogs/info.py
@@ -63,7 +63,7 @@ class Info(commands.Cog):
 
 			await ctx.send(embed = embed)
 		elif isinstance(error, commands.CommandOnCooldown):
-			embed = discord.Embed(title = "Command Error", description = f"You can use this command {error.cooldown.rate} time every {time.strftime('%M', time.gmtime(error.cooldown.per))} minutes. Retry after {time.strftime('%M', time.gmtime(error.retry_after))} seconds. :x:", colour = discord.Colour.red())
+			embed = discord.Embed(title = "Command Error", description = f"You can use this command {error.cooldown.rate} time every {time.strftime('%M', time.gmtime(error.cooldown.per))} minutes. Retry after {time.strftime('%M', time.gmtime(error.retry_after))} minutes. :x:", colour = discord.Colour.red())
 			embed.set_author(name = ctx.author, icon_url = str(ctx.author.avatar_url))
 
 			await ctx.send(embed = embed)
