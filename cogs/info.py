c90f865b029a7dbf49b4a459ed3a70d0a286fc80
Dan <danilvoytvoich07@gmail.com>
2020-08-19 15:06:30 +0300

    update

--- cogs/info.py
+++ cogs/info.py
@@ -126,7 +126,7 @@ class Info(commands.Cog):
 		embed_info = discord.Embed(name = "Bot's Info", description = f"Using discord.py {discord.__version__}", colour = discord.Colour.green())
 
 		embed_info.add_field(name = "Ping", value = f"{round(self.bot.latency * 1000)}ms", inline = False)
-		embed_info.add_field(name = "Memory Usage", value = f"{round(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)}MB / {round(psutil.virtual_memory().available / 1024 ** 2)}MB", inline = False)
+		embed_info.add_field(name = "Memory Usage", value = f"{round(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)}MB / {round(psutil.virtual_memory().total / 1024 ** 2)}MB", inline = False)
 		embed_info.add_field(name = "Bot's Version", value = BOT_VERSION, inline = False)
 		embed_info.add_field(name = "Servers", value = len(self.bot.guilds), inline = False)
 		embed_info.add_field(name = "Users", value = len(self.bot.users), inline = False)
